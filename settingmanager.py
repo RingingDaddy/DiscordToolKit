03c0666106793d6534187cf943b9683ad0ac6afa
v3s <v3s_kb@gmail.com>
2021-05-20 17:58:54 +0200

    Add a function for getting config file settings.

--- settingmanager.py
+++ settingmanager.py
@@ -8,16 +8,22 @@ this = sys.modules[__name__]
 
 this.alttoken = None
 this.maintoken = None
+this.configfile = None
 
 def loadsettings():
     with open("settings.json") as file:
-        configfile = json.loads(file.read())
+        this.configfile = json.loads(file.read()) #Load config file into memory.
         file.close() # Release config file.
 
-    this.maintoken = configfile['tokens']['main']
-    this.alttoken  = configfile['tokens']['alt']
-    print(maintoken)
-    
+def get_setting(field, field2 = None):
+    #TODO: make this not be extremely stupid
+    if(field2 != None):
+        print("configfile[{}][{}]".format(field, field2))
+        return configfile[field][field2]
+    else:
+        return configfile[field]
+
+
 
 def __main__():
     loadsettings()
