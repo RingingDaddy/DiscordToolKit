100a071ea5d7d5977407bef7eb25d7057597e30f
Veestire <Veestire@gmail.com>
2021-05-22 14:20:47 +0200

    Add message logging, and also remove my alt's token from the github lmfao

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import threading
 from colorama import Back, Fore, Style
 
 
-
 class colors:
     """USAGE: print(colors.COLOR + TEXT + color.NONE) """
     CYAN = '\033[96m'
     RED = '\033[91m'
     YELLOW = '\033[93m'
     NONE = '\033[0m'
+
 
 def toolkitexit():
     print(f"{Fore.BLACK}{Back.YELLOW}Exitting...{Style.RESET_ALL}")
@@ -40,5 +40,4 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"{Fore.BLACK}{Back.RED}{e}{Style.RESET_ALL}")
 
-    toolkitexit() #This will only execute if the selfbot crashes for some reason, or if you turn off the app using CTRL + C
-    
+    toolkitexit()  # This will only execute if the selfbot crashes for some reason, or if you turn off the app using CTRL + C
--- selfbot.py
+++ selfbot.py
@@ -9,10 +9,6 @@ from discord.ext import commands
 client = discord.Client()
 
 
-def __init__():
-    this.client = client
-
-
 @client.event
 async def on_ready():
     print("Selfbot ready.")
@@ -25,16 +21,26 @@ async def on_ready():
 @client.event
 async def on_message(message):
     now = datetime.datetime.now()
+
     # Workaround around selfbots being prevented from reading message content not sent by themselves.
     # Instead of accessing .content from the message object, this checks the latest message in message.channel, which results in the same result as message.content
     # This is slightly slower, but works for now.
-    if(menu.logstate):
-        async for message in message.channel.history(limit=1):
-            if(settingmanager.get_setting("logger", "filterbotmessages") == "True" and message.author.bot == True):
-                pass
-            else:
-                print(f"{Fore.BLACK}{Back.CYAN}" + "[{}:{}:{}] {}: {} | Server: {}".format(
-                    now.hour, now.minute, now.second, message.author, message.content, message.guild.name) + Back.RESET)
+
+    async for message in message.channel.history(limit=1):
+        if(settingmanager.get_setting("logger", "filterbotmessages") == "True" and message.author.bot == True):
+            pass
+        else:
+            # This looks terrible now, i have no clue how to fix this...
+            messagecontent = "[{:02d}:{:02d}:{:02d}] {}: {} | Server: {}".format(
+                now.hour, now.minute, now.second, message.author, message.content, message.guild.name)
+
+            if(settingmanager.get_setting("logger", "logfile") != ""):
+                with open(settingmanager.get_setting("logger", "logfile"), 'a') as messages:
+                    messages.write(messagecontent + '\n')
+
+            if(menu.logstate):
+                print(f"{Fore.BLACK}{Back.CYAN}" +
+                      messagecontent + Style.RESET_ALL)
 
 
 def start():
